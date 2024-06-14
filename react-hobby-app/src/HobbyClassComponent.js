4ef2c81ca2a4e1d9997b0bca9d6ffaa0a66624f6
alex-cx-v <alex.cx.volkov@gmail.com>
2024-06-14 13:38:12 +0300

    Додано файли

---
+++ react-hobby-app/src/HobbyClassComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HobbyClassComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Моє хоббі</h1>
+                <p>Я люблю займатися риболовлею.</p>
+            </div>
+        );
+    }
+}
+
+export default HobbyClassComponent;
