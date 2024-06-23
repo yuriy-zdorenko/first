a1b4ebfe54094eb140c0089ff412144d1bc3657b
ShyraiDan <dan.shirayy@gmail.com>
2024-06-23 22:25:56 +0300

    lab19

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 80,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  endOfLine: 'auto',
+  semicolons: true,
+};
